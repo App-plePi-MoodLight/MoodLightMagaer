10478fcfae860c558af17a409426af2612666789
inseong04 <ois0531@naver.com>
2021-11-19 00:15:04 +0900

    when user create question, fixed that question was created three question

--- app/src/main/java/com/example/moodlightmanger/QuestionCreateActivity.kt
+++ app/src/main/java/com/example/moodlightmanger/QuestionCreateActivity.kt
@@ -7,6 +7,9 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.example.moodlight.api.ServerClient
 import com.example.moodlightmanger.model.QuestionCreateModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,30 +21,82 @@ class QuestionCreateActivity : AppCompatActivity() {
 
         findViewById<AppCompatButton>(R.id.create_btn)
             .setOnClickListener {
-                ServerClient.getApiService().createQuestion(
-                    QuestionCreateModel("happy",findViewById<EditText>(R.id.date_etv).text.toString(),
-                        findViewById<EditText>(R.id.content_etv).text.toString())
-                ).enqueue(object : Callback<QuestionCreateModel> {
-
-                    override fun onResponse(
-                        call: Call<QuestionCreateModel>,
-                        response: Response<QuestionCreateModel>,
-                    ) {
-                        if (response.isSuccessful) {
-                            Toast.makeText(applicationContext, "성공적으로 등록되었습니다.", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                        else {
-                            Toast.makeText(applicationContext, response.code().toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onFailure(call: Call<QuestionCreateModel>, t: Throwable) {
-                        t.printStackTrace()
-                    }
-
-                })
+                createQuestion(findViewById<EditText>(R.id.date_etv).text.toString(),
+                    findViewById<EditText>(R.id.content_etv).text.toString())
             }
+
+    }
+
+    private fun createQuestion(date : String, contents : String) {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            ServerClient.getApiService().createQuestion(
+                QuestionCreateModel("angry",date,
+                    contents)
+            ).enqueue(object : Callback<QuestionCreateModel> {
+
+                override fun onResponse(
+                    call: Call<QuestionCreateModel>,
+                    response: Response<QuestionCreateModel>,
+                ) {
+                    if (!response.isSuccessful) {
+
+                        Toast.makeText(applicationContext, response.code().toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<QuestionCreateModel>, t: Throwable) {
+                    t.printStackTrace()
+                }
+
+            })
+
+            ServerClient.getApiService().createQuestion(
+                QuestionCreateModel("sad",date,
+                    contents)
+            ).enqueue(object : Callback<QuestionCreateModel> {
+
+                override fun onResponse(
+                    call: Call<QuestionCreateModel>,
+                    response: Response<QuestionCreateModel>,
+                ) {
+                    if (!response.isSuccessful) {
+
+                        Toast.makeText(applicationContext, response.code().toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<QuestionCreateModel>, t: Throwable) {
+                    t.printStackTrace()
+                }
+
+            })
+        }
+
+        ServerClient.getApiService().createQuestion(
+            QuestionCreateModel("happy",date,
+                contents)
+        ).enqueue(object : Callback<QuestionCreateModel> {
+
+            override fun onResponse(
+                call: Call<QuestionCreateModel>,
+                response: Response<QuestionCreateModel>,
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(applicationContext, "성공적으로 등록되었습니다.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+                else {
+                    Toast.makeText(applicationContext, response.code().toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<QuestionCreateModel>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+
 
     }
 }
